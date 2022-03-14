7387b2906065d30fedd892768115990a09a76d5b
Haris Shah <harisshah385@gmail.com>
2022-03-14 16:44:18 +0500

    add blog card

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,10 +1,24 @@
+import { images } from '../constants';
 import { Blog } from '../container';
+
+const blogData = {
+	id: 1,
+	img: images.heroImg,
+	title: '',
+	subtitle: '',
+	time: '',
+};
 
 const Blogs = () => {
 	return (
-		<div>
-			<Blog />
-		</div>
+		<section>
+			<Blog
+				img={blogData.img}
+				title={blogData.title}
+				subtitle={blogData.subtitle}
+				time={blogData.time}
+			/>
+		</section>
 	);
 };
 
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,12 +1,19 @@
+import { images } from '../../constants';
 import styles from './BlogCard.module.css';
 
-const BlogCard = () => {
+const BlogCard = ({ img, time, title, subtitle }) => {
 	return (
-		<a href='/link' className={styles.card}>
-			<img src='' alt='img' className={styles.card__img} />
-			<span className={styles.card__time}>Time here</span>
-			<h3 className={styles.card__title}>Blog title here</h3>
-			<p className={styles.card__subtitle}>Blog subtitle here</p>
+		<a href='/blog-link' className={styles.card}>
+			<img src={images.heroImg} alt='img' className={styles.card__img} />
+			<h3 className={styles.card__title}>
+				See The Light: What’s in The Amazon Lightning Deals
+			</h3>
+			<p className={styles.card__subtitle}>
+				Blog subtitle here Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Corporis consectetur placeat cumque consequatur aliquam nemo illo
+				accusamus modi possimus soluta!
+			</p>
+			<p className={styles.card__continue}>Continue Reading...</p>
 		</a>
 	);
 };
