5d1486e9fa4f279fd1ef5d1670dd1850196c26f1
Haris Shah <harisshah385@gmail.com>
2022-04-11 21:49:22 +0500

    test the vps

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,7 @@ import Faq from '../container/Faq/Faq';
 import { images } from '../constants';
 import { motion } from 'framer-motion';
 const home = {
-	title: 'Wanna Sell More!',
+	title: "Wanna Sell More! let's go",
 	subtitle:
 		'With Svetro take your online business to the Next level, By having a competitive edge with the most skilled and capable teams by your side of Amazon and E-commerce experts of every platform.',
 	btntext: 'Get a free Consultation',
