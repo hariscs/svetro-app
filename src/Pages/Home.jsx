51491da043b90461890b766e793eebf668a50c1d
tayyab2000 <62956515+tayyab2000@users.noreply.github.com>
2022-11-28 01:20:47 -0800

    Update Home.jsx
    Update title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,7 @@ import Faq from '../container/Faq/Faq';
 import { images } from '../constants';
 import { motion } from 'framer-motion';
 const home = {
-	title: 'Want to Sell More!',
+	title: 'Build your online asesst with Svetro!',
 	subtitle:
 		'With Svetro take your online business to the Next level, By having a competitive edge with the most skilled and capable teams by your side of Amazon and E-commerce experts of every platform. The business is just like a game, one should be a player to play it smart and there is never a Game over.',
 	btntext: 'Get a free Consultation',
