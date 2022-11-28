b4c61b517167b85ee154c996f987e2c46955e41f
tayyab2000 <62956515+tayyab2000@users.noreply.github.com>
2022-11-28 04:13:31 -0800

    Update Home.jsx
    Spelling update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,7 @@ import Faq from '../container/Faq/Faq';
 import { images } from '../constants';
 import { motion } from 'framer-motion';
 const home = {
-	title: 'Build your online asesst with Svetro!',
+	title: 'Build your online asset with Svetro!',
 	subtitle:
 		'With Svetro take your online business to the Next level, By having a competitive edge with the most skilled and capable teams by your side of Amazon and E-commerce experts of every platform. The business is just like a game, one should be a player to play it smart and there is never a Game over.',
 	btntext: 'Get a free Consultation',
