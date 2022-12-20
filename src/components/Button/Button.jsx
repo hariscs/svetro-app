a1b557ea6ad086620b7bab748c3d9fca7c399526
tayyab2000 <62956515+tayyab2000@users.noreply.github.com>
2022-12-20 00:46:03 -0800

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const Button = ({ children, btnType }) => {
-  return (
-    <div>
-      <script src="https://asset-tidycal.b-cdn.net//js/embed.js"></script>
-      <div id="tidycal-embed" data-path="svetro/book-a-meeting"></div>
-    </div>
-  );
+	return (
+		<a href='https://tidycal.com/svetro/book-a-meeting' className={`btn ${btnType}`}>
+			{children}
+		</a>
+	);
 };
 
 export default Button;
